823bddd6cd7adaf103a5223a123b87d66c54eb0e
Jared Hanson <jaredhanson@gmail.com>
2013-11-07 15:46:12 -0800

    Test case for top-level resource with only some actions.

--- test/router.resource.test.js
+++ test/router.resource.test.js
@@ -130,4 +130,81 @@ describe('Router#resource', function() {
     });
   });
   
+  describe('top-level resource with only some actions', function() {
+    var app, router;
+    
+    before(function() {
+      app = new MockApplication();
+      router = new Router(handler);
+      router.define(function(method, path, handler) {
+        app[method](path, handler);
+      });
+      router.assist(function(name, entry) {
+        app.helper(name, entry);
+      });
+      
+      router.resource('profile', { only: [ 'show', 'edit', 'update' ] });
+    })
+    
+    it('should define application routes', function() {
+      expect(Object.keys(app.map)).to.have.length(2);
+      expect(app.map['get']).to.be.an('array');
+      expect(app.map['get']).to.have.length(2);
+      expect(app.map['put']).to.be.an('array');
+      expect(app.map['put']).to.have.length(1);
+    });
+    
+    it('should create route to show action', function() {
+      var route = app.map['get'][0];
+      expect(route.path).to.equal('/profile.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('profile');
+      expect(rv.action).to.equal('show');
+    });
+    
+    it('should create route to edit action', function() {
+      var route = app.map['get'][1];
+      expect(route.path).to.equal('/profile/edit.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('profile');
+      expect(rv.action).to.equal('edit');
+    });
+    
+    it('should create route to update action', function() {
+      var route = app.map['put'][0];
+      expect(route.path).to.equal('/profile');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('profile');
+      expect(rv.action).to.equal('update');
+    });
+    
+    it('should define application helpers', function() {
+      expect(Object.keys(app.helpers)).to.have.length(2);
+    });
+    
+    it('should register show helper for route', function() {
+      var entry = app.helpers['profile'];
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/profile.:format?');
+      expect(entry.controller).to.equal('profile');
+      expect(entry.action).to.equal('show');
+    });
+    
+    it('should register edit helper for route', function() {
+      var entry = app.helpers['editProfile'];
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/profile/edit.:format?');
+      expect(entry.controller).to.equal('profile');
+      expect(entry.action).to.equal('edit');
+    });
+  });
+  
 });
