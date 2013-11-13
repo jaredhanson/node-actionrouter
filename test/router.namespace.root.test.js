1aefd9df313fd4da66dfca0b94a4ea7eca367b7e
Jared Hanson <jaredhanson@gmail.com>
2013-11-12 21:18:52 -0800

    Test case for namespace with root resource.

---
+++ test/router.namespace.root.test.js
@@ -0,0 +1,44 @@
+var Router = require('../lib/router')
+  , MockApplication = require('./mocks/mockapplication');
+  
+
+describe('Router#namespace', function() {
+  
+  function handler(controller, action) {
+    return function() {
+      return { controller: controller, action: action };
+    }
+  }
+
+  describe('namespace with root resource', function() {
+    var app, router;
+    
+    before(function() {
+      app = new MockApplication();
+      router = new Router(handler);
+      router.define(function(method, path, handler) {
+        app[method](path, handler);
+      });
+      
+      router.namespace('top40', function() {
+        router.root('pages#main');
+      });
+    })
+    
+    it('should define application routes', function() {
+      expect(app.map['get']).to.be.an('array');
+      expect(app.map['get']).to.have.length(1);
+    });
+    
+    it('should create route to controller action', function() {
+      var route = app.map['get'][0];
+      expect(route.path).to.equal('/top40');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('top40/pages');
+      expect(rv.action).to.equal('main');
+    });
+  });
+
+});
