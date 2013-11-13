5d7c8526f4e93a0a5eacefffd5d52cec4d9c1dc8
Jared Hanson <jaredhanson@gmail.com>
2013-11-12 22:14:02 -0800

    Test case for root route declared with object notation.

--- test/router.root.test.js
+++ test/router.root.test.js
@@ -39,4 +39,33 @@ describe('Router#root', function() {
     });
   });
   
+  describe('object notation', function() {
+    var app, router;
+    
+    before(function() {
+      app = new MockApplication();
+      router = new Router(handler);
+      router.define(function(method, path, handler) {
+        app[method](path, handler);
+      });
+      
+      router.root({ controller: 'pages', action: 'home' });
+    })
+    
+    it('should define application routes', function() {
+      expect(app.map['get']).to.be.an('array');
+      expect(app.map['get']).to.have.length(1);
+    });
+    
+    it('should create route to controller action', function() {
+      var route = app.map['get'][0];
+      expect(route.path).to.equal('/');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('pages');
+      expect(rv.action).to.equal('home');
+    });
+  });
+  
 });
