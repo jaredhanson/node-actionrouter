4d0c320db586f436836b9c0f8390ce8ef58d5a9e
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 10:22:35 -0800

    More test cases for nesting resources.

--- test/router.resources.nesting.test.js
+++ test/router.resources.nesting.test.js
@@ -682,6 +682,360 @@ describe('Router#resources', function() {
     });
   });
   
+  describe('resources using dasherized name with nested resources', function() {
+    var app, router;
+    
+    before(function() {
+      app = new MockApplication();
+      router = new Router(handler);
+      router.define(function(method, path, handler) {
+        app[method](path, handler);
+      });
+      router.assist(function(name, entry) {
+        app.helper(name, entry);
+      });
+      
+      router.resources('radio-stations', function() {
+        this.resources('albums');
+      });
+    });
+    
+    it('should define application routes', function() {
+      expect(Object.keys(app.map)).to.have.length(4);
+      expect(app.map['get']).to.be.an('array');
+      expect(app.map['get']).to.have.length(8);
+      expect(app.map['post']).to.be.an('array');
+      expect(app.map['post']).to.have.length(2);
+      expect(app.map['put']).to.be.an('array');
+      expect(app.map['put']).to.have.length(2);
+      expect(app.map['delete']).to.be.an('array');
+      expect(app.map['delete']).to.have.length(2);
+    });
+    
+    it('should create route to nested index action', function() {
+      var route = app.map['get'][4];
+      expect(route.path).to.equal('/radio-stations/:radio_station_id/albums.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('index');
+    });
+    
+    it('should create route to nested new action', function() {
+      var route = app.map['get'][5];
+      expect(route.path).to.equal('/radio-stations/:radio_station_id/albums/new.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('new');
+    });
+    
+    it('should create route to nested create action', function() {
+      var route = app.map['post'][1];
+      expect(route.path).to.equal('/radio-stations/:radio_station_id/albums');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('create');
+    });
+    
+    it('should create route to nested show action', function() {
+      var route = app.map['get'][6];
+      expect(route.path).to.equal('/radio-stations/:radio_station_id/albums/:id.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('show');
+    });
+    
+    it('should create route to nested edit action', function() {
+      var route = app.map['get'][7];
+      expect(route.path).to.equal('/radio-stations/:radio_station_id/albums/:id/edit.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('edit');
+    });
+    
+    it('should create route to nested update action', function() {
+      var route = app.map['put'][1];
+      expect(route.path).to.equal('/radio-stations/:radio_station_id/albums/:id');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('update');
+    });
+    
+    it('should create route to nested destroy action', function() {
+      var route = app.map['delete'][1];
+      expect(route.path).to.equal('/radio-stations/:radio_station_id/albums/:id');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('destroy');
+    });
+    
+    it('should define application helpers', function() {
+      expect(Object.keys(app.helpers)).to.have.length(8);
+    });
+    
+    it('should register index helper for route', function() {
+      var entry = app.helpers.radioStations;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('index');
+    });
+    
+    it('should register show helper for route', function() {
+      var entry = app.helpers.radioStation;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations/:id.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('show');
+    });
+    
+    it('should register new helper for route', function() {
+      var entry = app.helpers.newRadioStation;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations/new.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('new');
+    });
+    
+    it('should register edit helper for route', function() {
+      var entry = app.helpers.editRadioStation;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations/:id/edit.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('edit');
+    });
+    
+    it('should register index helper for nested route', function() {
+      var entry = app.helpers.radioStationAlbums;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations/:radio_station_id/albums.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('index');
+    });
+    
+    it('should register show helper for nested route', function() {
+      var entry = app.helpers.radioStationAlbum;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations/:radio_station_id/albums/:id.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('show');
+    });
+    
+    it('should register new helper for nested route', function() {
+      var entry = app.helpers.newRadioStationAlbum;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations/:radio_station_id/albums/new.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('new');
+    });
+    
+    it('should register edit helper for nested route', function() {
+      var entry = app.helpers.editRadioStationAlbum;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio-stations/:radio_station_id/albums/:id/edit.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('edit');
+    });
+  });
+  
+  describe('resources using camel cased name with nested resources', function() {
+    var app, router;
+    
+    before(function() {
+      app = new MockApplication();
+      router = new Router(handler);
+      router.define(function(method, path, handler) {
+        app[method](path, handler);
+      });
+      router.assist(function(name, entry) {
+        app.helper(name, entry);
+      });
+      
+      router.resources('radio_stations', function() {
+        this.resources('albums');
+      });
+    });
+    
+    it('should define application routes', function() {
+      expect(Object.keys(app.map)).to.have.length(4);
+      expect(app.map['get']).to.be.an('array');
+      expect(app.map['get']).to.have.length(8);
+      expect(app.map['post']).to.be.an('array');
+      expect(app.map['post']).to.have.length(2);
+      expect(app.map['put']).to.be.an('array');
+      expect(app.map['put']).to.have.length(2);
+      expect(app.map['delete']).to.be.an('array');
+      expect(app.map['delete']).to.have.length(2);
+    });
+    
+    it('should create route to nested index action', function() {
+      var route = app.map['get'][4];
+      expect(route.path).to.equal('/radio_stations/:radio_station_id/albums.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('index');
+    });
+    
+    it('should create route to nested new action', function() {
+      var route = app.map['get'][5];
+      expect(route.path).to.equal('/radio_stations/:radio_station_id/albums/new.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('new');
+    });
+    
+    it('should create route to nested create action', function() {
+      var route = app.map['post'][1];
+      expect(route.path).to.equal('/radio_stations/:radio_station_id/albums');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('create');
+    });
+    
+    it('should create route to nested show action', function() {
+      var route = app.map['get'][6];
+      expect(route.path).to.equal('/radio_stations/:radio_station_id/albums/:id.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('show');
+    });
+    
+    it('should create route to nested edit action', function() {
+      var route = app.map['get'][7];
+      expect(route.path).to.equal('/radio_stations/:radio_station_id/albums/:id/edit.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('edit');
+    });
+    
+    it('should create route to nested update action', function() {
+      var route = app.map['put'][1];
+      expect(route.path).to.equal('/radio_stations/:radio_station_id/albums/:id');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('update');
+    });
+    
+    it('should create route to nested destroy action', function() {
+      var route = app.map['delete'][1];
+      expect(route.path).to.equal('/radio_stations/:radio_station_id/albums/:id');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('albums');
+      expect(rv.action).to.equal('destroy');
+    });
+    
+    it('should define application helpers', function() {
+      expect(Object.keys(app.helpers)).to.have.length(8);
+    });
+    
+    it('should register index helper for route', function() {
+      var entry = app.helpers.radioStations;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('index');
+    });
+    
+    it('should register show helper for route', function() {
+      var entry = app.helpers.radioStation;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations/:id.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('show');
+    });
+    
+    it('should register new helper for route', function() {
+      var entry = app.helpers.newRadioStation;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations/new.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('new');
+    });
+    
+    it('should register edit helper for route', function() {
+      var entry = app.helpers.editRadioStation;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations/:id/edit.:format?');
+      expect(entry.controller).to.equal('radioStations');
+      expect(entry.action).to.equal('edit');
+    });
+    
+    it('should register index helper for nested route', function() {
+      var entry = app.helpers.radioStationAlbums;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations/:radio_station_id/albums.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('index');
+    });
+    
+    it('should register show helper for nested route', function() {
+      var entry = app.helpers.radioStationAlbum;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations/:radio_station_id/albums/:id.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('show');
+    });
+    
+    it('should register new helper for nested route', function() {
+      var entry = app.helpers.newRadioStationAlbum;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations/:radio_station_id/albums/new.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('new');
+    });
+    
+    it('should register edit helper for nested route', function() {
+      var entry = app.helpers.editRadioStationAlbum;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/radio_stations/:radio_station_id/albums/:id/edit.:format?');
+      expect(entry.controller).to.equal('albums');
+      expect(entry.action).to.equal('edit');
+    });
+  });
+  
   describe('resources using camel cased name with nested resources', function() {
     var app, router;
     
