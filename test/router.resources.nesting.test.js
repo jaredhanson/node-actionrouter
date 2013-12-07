f3f46b72d0afeeaea27fee0cc5285ca3c24a9fca
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 11:29:17 -0800

    Implement param option to nested resources.

--- test/router.resources.nesting.test.js
+++ test/router.resources.nesting.test.js
@@ -1213,4 +1213,181 @@ describe('Router#resources', function() {
     });
   });
   
+  describe('resources with nested resources using param option', function() {
+    var app, router;
+    
+    before(function() {
+      app = new MockApplication();
+      router = new Router(handler);
+      router.define(function(method, path, handler) {
+        app[method](path, handler);
+      });
+      router.assist(function(name, entry) {
+        app.helper(name, entry);
+      });
+      
+      router.resources('vendors', { param: 'vendorOid' }, function() {
+        this.resources('devices', { param: 'serialNumber' });
+      });
+    });
+    
+    it('should define application routes', function() {
+      expect(Object.keys(app.map)).to.have.length(4);
+      expect(app.map['get']).to.be.an('array');
+      expect(app.map['get']).to.have.length(8);
+      expect(app.map['post']).to.be.an('array');
+      expect(app.map['post']).to.have.length(2);
+      expect(app.map['put']).to.be.an('array');
+      expect(app.map['put']).to.have.length(2);
+      expect(app.map['delete']).to.be.an('array');
+      expect(app.map['delete']).to.have.length(2);
+    });
+    
+    it('should create route to nested index action', function() {
+      var route = app.map['get'][4];
+      expect(route.path).to.equal('/vendors/:vendorOid/devices.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('devices');
+      expect(rv.action).to.equal('index');
+    });
+    
+    it('should create route to nested new action', function() {
+      var route = app.map['get'][5];
+      expect(route.path).to.equal('/vendors/:vendorOid/devices/new.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('devices');
+      expect(rv.action).to.equal('new');
+    });
+    
+    it('should create route to nested create action', function() {
+      var route = app.map['post'][1];
+      expect(route.path).to.equal('/vendors/:vendorOid/devices');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('devices');
+      expect(rv.action).to.equal('create');
+    });
+    
+    it('should create route to nested show action', function() {
+      var route = app.map['get'][6];
+      expect(route.path).to.equal('/vendors/:vendorOid/devices/:serialNumber.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('devices');
+      expect(rv.action).to.equal('show');
+    });
+    
+    it('should create route to nested edit action', function() {
+      var route = app.map['get'][7];
+      expect(route.path).to.equal('/vendors/:vendorOid/devices/:serialNumber/edit.:format?');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('devices');
+      expect(rv.action).to.equal('edit');
+    });
+    
+    it('should create route to nested update action', function() {
+      var route = app.map['put'][1];
+      expect(route.path).to.equal('/vendors/:vendorOid/devices/:serialNumber');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('devices');
+      expect(rv.action).to.equal('update');
+    });
+    
+    it('should create route to nested destroy action', function() {
+      var route = app.map['delete'][1];
+      expect(route.path).to.equal('/vendors/:vendorOid/devices/:serialNumber');
+      expect(route.handler).to.be.a('function');
+      
+      var rv = route.handler();
+      expect(rv.controller).to.equal('devices');
+      expect(rv.action).to.equal('destroy');
+    });
+    
+    it('should define application helpers', function() {
+      expect(Object.keys(app.helpers)).to.have.length(8);
+    });
+    
+    it('should register index helper for route', function() {
+      var entry = app.helpers.vendors;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors.:format?');
+      expect(entry.controller).to.equal('vendors');
+      expect(entry.action).to.equal('index');
+    });
+    
+    it('should register show helper for route', function() {
+      var entry = app.helpers.vendor;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors/:vendorOid.:format?');
+      expect(entry.controller).to.equal('vendors');
+      expect(entry.action).to.equal('show');
+    });
+    
+    it('should register new helper for route', function() {
+      var entry = app.helpers.newVendor;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors/new.:format?');
+      expect(entry.controller).to.equal('vendors');
+      expect(entry.action).to.equal('new');
+    });
+    
+    it('should register edit helper for route', function() {
+      var entry = app.helpers.editVendor;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors/:vendorOid/edit.:format?');
+      expect(entry.controller).to.equal('vendors');
+      expect(entry.action).to.equal('edit');
+    });
+    
+    it('should register index helper for nested route', function() {
+      var entry = app.helpers.vendorDevices;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors/:vendorOid/devices.:format?');
+      expect(entry.controller).to.equal('devices');
+      expect(entry.action).to.equal('index');
+    });
+    
+    it('should register show helper for nested route', function() {
+      var entry = app.helpers.vendorDevice;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors/:vendorOid/devices/:serialNumber.:format?');
+      expect(entry.controller).to.equal('devices');
+      expect(entry.action).to.equal('show');
+    });
+    
+    it('should register new helper for nested route', function() {
+      var entry = app.helpers.newVendorDevice;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors/:vendorOid/devices/new.:format?');
+      expect(entry.controller).to.equal('devices');
+      expect(entry.action).to.equal('new');
+    });
+    
+    it('should register edit helper for nested route', function() {
+      var entry = app.helpers.editVendorDevice;
+      
+      expect(entry).to.be.an('object');
+      expect(entry.pattern).to.equal('/vendors/:vendorOid/devices/:serialNumber/edit.:format?');
+      expect(entry.controller).to.equal('devices');
+      expect(entry.action).to.equal('edit');
+    });
+  });
+  
 });
